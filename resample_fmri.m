49d7cdee10511fa529c50863e4c9e3a29e4c172a
Jon Dudley <98111478+duddb3@users.noreply.github.com>
2024-09-05 10:13:54 -0400

    Adding the script

---
+++ resample_fmri.m
@@ -0,0 +1,106 @@
+function resample_fmri(examdir,tdir,dopad)
+    % If no input exam directory is specified, prompt user to select from
+    % exams on IRCDICOM
+    if ~exist('scandir','var')
+        list = dir('I:\15-400*');
+        [sel,ok] = listdlg('PromptString','Select scan to convert',...
+            'SelectionMode','single',...
+            'ListString',{list.name});
+        if ~ok
+            return
+        else
+            examdir = fullfile(list(sel).folder,list(sel).name);
+        end
+    end
+    % Get the list of scans with "fMRI" in the name from the exam directory
+    scans = dir(fullfile(examdir,'*','*fMRI*'));
+    rsscans = dir(fullfile(examdir,'*','*Resting_State*'));
+    scans = cat(1,scans,rsscans);
+    scans(~vertcat(scans.isdir)) = [];
+    if isempty(scans)
+        fprintf('No fMRI exams found in exam directory\n')
+        return
+    end
+    [subdir,datedir] = fileparts(scans(1).folder);
+    [~,subdir] = fileparts(subdir);
+
+    % If no input target directory is specified...
+    if ~exist('tdir','var')
+        % set default location if EXODUS is mapped as X:\
+        tdir = fullfile('X:\EXODUS\IRC1080_resampled');
+        if isfolder(tdir)
+            tdir = fullfile(tdir,subdir,datedir);
+        else
+            % otherwise prompt user to select a location
+            tdir = uigetdir('','Select location to save resampled images');
+            if ~ischar(tdir)
+                return
+            end
+        end
+    end
+
+    % If padding type not specified, set to true
+    if ~exist('dopad','var')
+        dopad = true;
+    end
+
+
+    % iterate thru each scan
+    for s=1:length(scans)
+
+        % Get the list of DICOM files in the given scan directory
+        dcms = dir(fullfile(scans(s).folder,scans(s).name,'*.dcm'));
+        
+        % Create a target directory for the downsampled DICOMs
+        ddir = fullfile(tdir,['ds_' scans(s).name]);
+        if ~isfolder(ddir)
+            mkdir(ddir)
+        end
+        
+        % Read in the first DICOM to ensure the expected resolution &
+        % matrix size
+        info = dicominfo(fullfile(dcms(1).folder,dcms(1).name));
+        if ~(all(info.PixelSpacing==[2.25;2.25]) && all([info.Rows;info.Columns]==[96;96]))
+            fprintf(2,'Unexpected acquisition parameters: contact Jon Dudley (jonathan.dudley@cchmc.org)\n')
+            return
+        end
+        
+        
+        parfor n=1:length(dcms)
+            % read in the DICOM tags
+            info = dicominfo(fullfile(dcms(n).folder,dcms(n).name));
+            % read in the image data
+            I = dicomread(info);
+        
+            % rescale to double
+            I = double(I).*info.RescaleSlope+info.RescaleIntercept;
+        
+            % resample to 90x90 matrix
+            rI = imresize(I,[90 90]);
+        
+            % rescale back to integers
+            rI = (rI-info.RescaleIntercept)./info.RescaleSlope;
+            rI = uint16(round(rI));
+        
+            % duplicate metadata
+            rinfo = info;
+            rinfo.PixelSpacing = [2.4;2.4]; % update resolution
+            if dopad
+                % pad the array
+                rI = cat(1,uint16(zeros(3,90)),rI,uint16(zeros(3,90)));
+                rI = cat(2,uint16(zeros(96,3)),rI,uint16(zeros(96,3)));
+                % adjust ImagePositionPatient accordingly
+                offset = 7.2;   % 3*2.4
+                rinfo.ImagePositionPatient(1) = rinfo.ImagePositionPatient(1)-offset;
+                rinfo.ImagePositionPatient(2) = rinfo.ImagePositionPatient(2)-offset;
+            else
+                % adjust the rows and columns to be 90x90
+                rinfo.Rows = 90;
+                rinfo.Columns = 90;
+            end
+            
+            % write the file
+            dicomwrite(rI,fullfile(ddir,dcms(n).name),rinfo,'CreateMode','copy','WritePrivate',true);
+        end
+    end
+end
